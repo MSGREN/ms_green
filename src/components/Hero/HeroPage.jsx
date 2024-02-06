e6a08e1d94ce0235fb3bb7e7d52d63f4a344efc2
olmonkasshu <olmontechdesign@gmail.com>
2024-02-06 15:15:46 +0100

    btn = new

--- src/components/Hero/HeroPage.jsx
+++ src/components/Hero/HeroPage.jsx
@@ -54,8 +54,7 @@ const HeroPage = () => {
           a playful token, a tasty potential.Sweeten your <br /> portfolio today! 🍬🪙🚀
         </p>
 
-        {/* Your btn goes here */}
-        <button className="ring-2 bg-green-900 ring-green-500 group-hovering:ring-1 transition duration-300 text-white px-3 py-2 m-2 md:text-sm rounded md:font-sm hover:bg-green-40" onClick={openModal}>
+        <button className="ring-1 bg-green-900 ring-lime-500 group-hovering:ring-1 transition duration-300 text-white px-4 py-2 m-2 md:text-sm rounded md:font-sm hover:ring-white" onClick={openModal}>
             BUY TOKEN
           </button>
           <Modal isOpen={modalOpen} onClose={closeModal} />
